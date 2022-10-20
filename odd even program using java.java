d12893b039d3a538dee885b78e44d8dc447d6c50
mohdkaif1234 <108331054+mohdkaif1234@users.noreply.github.com>
2022-10-20 14:01:42 +0530

    Add files via upload

---
+++ odd even program using java.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main
+{
+    
+  public static void main(string[] args){
+    
+    Scanner reader=new Scanner (System.in);
+
+    System.out.println("enter the number a:");
+
+    a=reader.nextInt();
+
+    if(a%2==0)
+
+    System.out.println("number is even a=" +a);
+
+    else
+
+    System.out.println("number is odd a=" +a);
+
+    }
+
+ 
+}
