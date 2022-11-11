3f9ee84df46d476b15db171ac957c284417f28ad
mohdkaif1234 <108331054+mohdkaif1234@users.noreply.github.com>
2022-11-11 11:44:42 +0530

    Add files via upload

---
+++ String .java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String args[]){
+
+        String name;
+
+        Scanner obj=new Scanner(System.in);
+
+        name=obj.nextLine();
+
+       System.out.print(name.charAt(5));
+
+    }
+
+}
