710063dfb7f5472f286d8ab48fb71ea323ecb0ce
mohdkaif1234 <108331054+mohdkaif1234@users.noreply.github.com>
2022-11-11 14:31:19 +0530

    Add files via upload

---
+++ checking  the number.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main
+{
+
+	public static void main(String[] args) {
+
+	    
+Scanner obj=new Scanner(System.in);
+
+	    int  a= obj.nextInt();
+
+	    int  b= obj.nextInt();
+
+	    if (a >b){
+
+	        System.out.println("a is greater b ");
+
+	  	}
+
+	     if (a==b){
+
+	  	    System.out.println("a is equal to b ");
+
+	  	}
+
+	     if (b>a){
+
+	  	    System.out.println("b is greater a ");
+
+	  	}
+
+	  	
+
+	  }
+
+}
