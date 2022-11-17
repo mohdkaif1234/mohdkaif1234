4f360fd3121338079b7e2b1e047dbb3edff92152
mohdkaif1234 <108331054+mohdkaif1234@users.noreply.github.com>
2022-11-17 10:13:58 +0530

    Add files via upload

---
+++ to find number of vowels char in a sentence.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main
+{
+
+	public static void main(String[] args) {
+
+	    int count=0;
+
+	    String sentence;
+
+	    System.out.println("Enter a sentence");
+
+	    Scanner obj=new Scanner(System.in);
+
+	    sentence=obj.nextLine();
+
+	    for(int i=0;i<sentence.length();i++){
+
+               char ch = sentence.charAt(i);
+
+		    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+
+		        count++;
+
+		    }
+
+	    }
+
+	    System.out.println("number of vowels in the given sentence is :"+count);
+
+	}
+
+}
