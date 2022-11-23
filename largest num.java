7f7732f5261a200e51b8e09373b206507fdd3894
mohdkaif1234 <108331054+mohdkaif1234@users.noreply.github.com>
2022-11-23 11:06:59 +0530

    Add files via upload

---
+++ largest num.java
@@ -0,0 +1,39 @@
+import java.util.*;
+ 
+public class Main  {
+ 
+   public static void main(String[] args) {
+
+        int a, b,c;
+
+        Scanner obj=new Scanner(System.in)
+
+        a=obj.nextInt();
+
+        b=obj.nextInt();
+
+       c=obj.nextInt();
+        if (a>b&& b>c && a>c)
+
+        {
+  
+          System.out.println("largest number\n"+a);
+
+        }
+ 
+           else if (b>c){
+  
+          System.out.println("largest number\n"+b);
+ 
+           }
+
+            else{
+ 
+               System.out.println("largest number\n"+c);
+
+            
+            }
+    
+}
+}        
+       
